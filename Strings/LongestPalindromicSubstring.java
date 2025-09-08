2637fb96e098167153e3485083156367df8e1dc2
ritesh <riteshswain1973@gmail.com>
2025-09-08 23:41:17 +0530

    Longest Palindromic Substring - Expand Around Center Approch

--- Strings/LongestPalindromicSubstring.java
+++ Strings/LongestPalindromicSubstring.java
@@ -2,8 +2,12 @@ public class LongestPalindromicSubstring {
     public static void main(String[] args) {
         String s = "babad";
         String result = longestPalindrome(s);
+        String result2 = longestPalindrome2(s);
         System.out.println("Longest palindromic substring: " + result); // Output: "bab" or "aba"
+        System.out.println("Longest palindromic substring: " + result2); // Output: "bab" or "aba"
+
     }
+    // Brute Force Approach
     private static  boolean ispalin(int i,int j,String s){
         while(i<j){
             if(s.charAt(i)!=s.charAt(j)) return false;
@@ -24,5 +28,28 @@ public class LongestPalindromicSubstring {
             }
         }
         return res;
+    }
+
+    // Expand Around Center
+    public static String longestPalindrome2(String s) {
+        if(s==null || s.length()<2) return s;
+        int st=0,end=0;
+        for(int i=0; i<s.length(); i++){
+            int even=centre(s,i,i);
+            int odd=centre(s,i,i+1);
+            int len=Math.max(even,odd);
+            if(len>end-st){
+                st=i-(len-1)/2;
+                end=i+len/2;
+            }
+        }
+        return s.substring(st,end+1);
+    }
+    private static  int centre(String s, int st, int end) {
+        while(st>=0 && end<s.length() && s.charAt(st)==s.charAt(end)) {
+            st--;
+            end++;
+        }
+        return end-st-1;
     }
 }
