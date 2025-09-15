1dd375e9498802f5b9c638958446df60226381ae
ritesh <riteshswain1973@gmail.com>
2025-09-15 23:58:53 +0530

    Majority Element

---
+++ Array/MajorityElement.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class MajorityElement{
+    public static void main(String[] args) {
+        int[] nums = {3,2,3};
+        System.out.println(majorityElement(nums));
+    }
+    public static int majorityElement(int[] nums) {
+        Map<Integer,Integer> mp=new HashMap<>();
+        for(int i:nums){
+            mp.put(i,mp.getOrDefault(i,0)+1);
+        }
+        for(var i:mp.entrySet()){
+            if(i.getValue()>nums.length/2) return i.getKey();
+        }
+        return -1;
+    }
+
+}
