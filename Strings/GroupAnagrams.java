bf8fcc4b1feddd87979bd3a102c03c5f4818e50e
ritesh <riteshswain1973@gmail.com>
2025-10-04 23:55:13 +0530

    Group Anagrams

--- Strings/GroupAnagrams.java
+++ Strings/GroupAnagrams.java
@@ -8,8 +8,9 @@ public class GroupAnagrams {
     public static void main(String[] args) {
         String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
         System.out.println(groupAnagrams(strs));
+        System.out.println(groupAnagrams2(strs));
     }
-    //Method 1: Sorting each string and using it as a key
+    //Method 1: HashTable
     public static List<List<String>> groupAnagrams(String[] strs) {
         Map<String,List<String>> mp=new HashMap<>();
         for(String i:strs){
@@ -23,6 +24,19 @@ public class GroupAnagrams {
         }
         return new ArrayList<>(mp.values());
     }
-    
 
+    //Method 2: Sorting each string and using it as a key
+    public static List<List<String>> groupAnagrams2(String[] strs) {
+        Map<String,List<String>> mp=new HashMap<>();
+        for(String i:strs){
+            int[] freq=new int[26];
+            for(char j:i.toCharArray()){
+                freq[j-'a']++;
+            }
+            String s=Arrays.toString(freq);
+            mp.putIfAbsent(s,new ArrayList<>());
+            mp.get(s).add(i);
+        }
+        return new ArrayList<>(mp.values());
+    }
 }
