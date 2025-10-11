ee4ca0275d9ded5f328b50a91b28a9cf60414ecb
ritesh <riteshswain1973@gmail.com>
2025-10-12 01:03:41 +0530

    Trapping Rain Water

---
+++ Array/TrappingRainWater.java
@@ -0,0 +1,24 @@
+
+
+public class TrappingRainWater {
+    public static void main(String[] args) {
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trap(height)); // Output: 6
+    }
+    public static int trap(int[] height) {
+        int n=height.length;
+        if(n==0) return 0;
+        int[] left=new int[n];
+        left[0]=height[0];
+        for(int i=1; i<n; i++){
+            left[i]=Math.max(left[i-1],height[i]);
+        }
+        int right=height[n-1];
+        int water=0;
+        for(int i=n-1; i>=0; i--){
+            right=Math.max(right,height[i]);
+            water+=Math.min(right,left[i])-height[i];
+        }
+        return water;
+    }
+}
