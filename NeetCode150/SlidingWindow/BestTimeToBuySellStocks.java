93bdbcfbb30eb09948a0aadec344bf53bd01a1e1
ritesh <riteshswain1973@gmail.com>
2025-10-15 00:00:08 +0530

    Best Time to Buy & Sell Stocks

--- NeetCode150/SlidingWindow/BestTimeToBuySellStocks.java
+++ NeetCode150/SlidingWindow/BestTimeToBuySellStocks.java
@@ -2,6 +2,7 @@ public class BestTimeToBuySellStocks {
     public static void main(String[] args) {
         int[] prices = {7,1,5,3,6,4};
         System.out.println(maxProfit(prices)); // Output: 5
+        System.out.println(maxProfit2(prices)); // Output: 5
     }
     public static int maxProfit(int[] prices) {
         int l=0,r=1;
@@ -14,6 +15,15 @@ public class BestTimeToBuySellStocks {
                 l=r;
             }
             r++;
+        }
+        return maxProfit;
+    }
+    public static int maxProfit2(int[] prices) {
+        int maxProfit=0;
+        int minPrice=prices[0];
+        for(int i:prices){
+            maxProfit=Math.max(maxProfit,i-minPrice);
+            minPrice=Math.min(minPrice,i);
         }
         return maxProfit;
     }
