3801dde3240ba7b6fe337c18a9df26e1793489fa
ritesh <riteshswain1973@gmail.com>
2025-11-10 12:20:39 +0530

    Partition Equal Subset Sum

--- Subset/PartitionEqualSubsetSum.java
+++ Subset/PartitionEqualSubsetSum.java
@@ -3,12 +3,12 @@ public class PartitionEqualSubsetSum{
         int[] nums={1,5,11,5};
         System.out.println(canPartition(nums));
     }
-    public static boolean slove(int[] nums,int target,int idx,int curr1,int curr2){
+    public static boolean solve(int[] nums,int target,int idx,int curr1,int curr2){
         if(idx==nums.length) return curr1==curr2;
         // if(curr1>target && curr2>target) return false;
 
-        if(slove(nums,target,idx+1,curr1+nums[idx],curr2)) return true;
-        if(slove(nums,target,idx+1,curr1,curr2+nums[idx])) return true;
+        if(solve(nums,target,idx+1,curr1+nums[idx],curr2)) return true;
+        if(solve(nums,target,idx+1,curr1,curr2+nums[idx])) return true;
 
         return false;
        
@@ -16,6 +16,6 @@ public class PartitionEqualSubsetSum{
     public static boolean canPartition(int[] nums) {
         int target=0;
         for(int i:nums) target+=i;
-        return slove(nums,target,0,0,0);
+        return solve(nums,target,0,0,0);
     }
 }
