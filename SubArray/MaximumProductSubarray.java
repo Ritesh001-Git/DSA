1c41db444b232ac7f33603f375490a9423e63b0a
ritesh <riteshswain1973@gmail.com>
2025-11-23 23:55:55 +0530

    Maximum Product Subarray

--- SubArray/MaximumProductSubarray.java
+++ SubArray/MaximumProductSubarray.java
@@ -1,16 +1,17 @@
 public class MaximumProductSubarray {
     public static void main(String[] args) {
-        int[] nums={-2,0,-1};
+        int[] nums={2,3,-2,4};
         System.out.println(maxProduct(nums));
     }
     public static int maxProduct(int[]nums){
         int max=Integer.MIN_VALUE;
         int p=1;
-        for(int i:nums){
-            p*=i;
+        for(int i=0; i<nums.length; i++){
+            p*=nums[i];
             max=Math.max(p,max);
             if(p==0) p=1;
         }
+        p=1;
         for(int i=nums.length-1; i>=0; i--){
             p*=nums[i];
             max=Math.max(p,max);
