3af02143963c67518aa750b36a1a5ea6d2a61aa8
ritesh <riteshswain1973@gmail.com>
2025-11-27 00:24:07 +0530

    Smallest Integer Divisible by K

---
+++ Math/SmallestIntegerDivisiblebyK.java
@@ -0,0 +1,15 @@
+public class SmallestIntegerDivisiblebyK {
+    public static void main(String[] args) {
+        int k=17;
+        System.out.println(smallestRepunitDivByK(k));
+    }
+    public static int smallestRepunitDivByK(int k){
+        if(k%2==0 || k%5==0) return -1;
+        int r=0;
+        for(int i=1;i<=k; i++){
+            r=(r*10+1)%k;
+            if(r==0) return i;
+        }
+        return -1;
+    }
+}
