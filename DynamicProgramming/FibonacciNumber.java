cc4b1a032c11fe41ed868c816550ac805c7f0d56
ritesh <riteshswain1973@gmail.com>
2025-12-02 00:39:45 +0530

    Fibonacci Numbers

--- DynamicProgramming/FibonacciNumber.java
+++ DynamicProgramming/FibonacciNumber.java
@@ -8,6 +8,8 @@ public class FibonacciNumber{
         int[] dp=new int[n+1];
         Arrays.fill(dp,-1);
         System.out.println(fibo2(n,dp));
+
+        System.out.println(fibo3(n));
     }
     // Recursive Approach
     public static int fibo1(int n){
@@ -22,4 +24,15 @@ public class FibonacciNumber{
         return fibo2(n-2,dp)+fibo2(n-1,dp);
     }
 
+    // Tabulation DP (Bottom-Up DP)
+    public static int fibo3(int n) {
+        if(n==0 || n==1) return n;
+        int f=0,s=1;
+        for(int i=1; i<=n; i++){
+            int third=f+s;
+            f=s;
+            s=third;
+        }
+        return f;
+    }
 }
