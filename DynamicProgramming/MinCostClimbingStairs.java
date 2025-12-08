3ccea54bc55167ce00a7728728a05972eab09a3a
ritesh <riteshswain1973@gmail.com>
2025-12-09 01:33:53 +0530

    Min Cost Climbing Stairs

---
+++ DynamicProgramming/MinCostClimbingStairs.java
@@ -0,0 +1,16 @@
+public class MinCostClimbingStairs {
+    public static void main(String[] args) {
+        int[] cost={1,100,1,1,1,100,1,1,100,1};
+        System.out.println(minCostClimbingStairs(cost));
+    }
+    public static int minCostClimbingStairs(int[] cost) {
+        int n=cost.length;
+        int[] dp=new int[n+1];
+        dp[0]=0;
+        dp[1]=0;
+        for(int i=2; i<=n; i++){
+            dp[i]=Math.min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
+        }
+        return dp[n];
+    }
+}
