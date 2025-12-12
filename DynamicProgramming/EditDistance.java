861d99b4ffbb7de1917b7631c1631c41f0cf9a65
ritesh <riteshswain1973@gmail.com>
2025-12-13 01:34:40 +0530

    Edit Distance

--- DynamicProgramming/EditDistance.java
+++ DynamicProgramming/EditDistance.java
@@ -1,5 +1,17 @@
 public class EditDistance {
     public static void main(String[] args) {
-        
+        String word1 = "intention", word2 = "execution";
+        System.out.println(minDistance(word1,word2));
+    }
+
+    // Recursion (TLE)
+    public static int minDistance(String word1,String word2){
+        return solve(0,0,word1,word2);
+    }
+    public static int solve(int i,int j,String word1,String word2){
+        if(word1.length()==i) return word2.length()-j;
+        if(word2.length()==j) return word1.length()-i;
+        if(word1.charAt(i)==word2.charAt(j)) return solve(i+1,j+1,word1,word2);
+        return 1+Math.min(solve(i+1,j+1,word1, word2),Math.min(solve(i+1,j,word1,word2),solve(i,j+1,word1,word2)));
     }
 }
