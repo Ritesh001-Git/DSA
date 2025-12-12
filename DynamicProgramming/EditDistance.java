d4264d61d3b55347929187fa13c075a0a4c041a6
ritesh <riteshswain1973@gmail.com>
2025-12-13 01:50:45 +0530

    Edit Distance

--- DynamicProgramming/EditDistance.java
+++ DynamicProgramming/EditDistance.java
@@ -5,6 +5,7 @@ public class EditDistance {
         String word1 = "intention", word2 = "execution";
         System.out.println(minDistanceRecursion(word1,word2));
         System.out.println(minDistance(word1,word2));
+        System.out.println(minDistance1(word1,word2));     
     }
 
     // Recursion (TLE)
@@ -32,5 +33,27 @@ public class EditDistance {
         if(dp[i][j]!=-1) return dp[i][j];
         if(word1.charAt(i)==word2.charAt(j)) return dp[i][j]=solve(i+1,j+1,word1,word2,dp);
         return dp[i][j]=1+Math.min(solve(i+1,j+1,word1, word2,dp),Math.min(solve(i+1,j,word1,word2,dp),solve(i,j+1,word1,word2,dp)));
+    }
+
+    // Tabulation DP (Bottom-Up DP)
+    public static int minDistance1(String word1,String word2){
+        int m=word1.length(),n=word2.length();
+        int[][] dp=new int[m+1][n+1];
+        for(int i=0; i<=m; i++){
+            dp[0][i]=i;
+        }
+        for(int j=0; j<=n; j++){
+            dp[j][0]=j;
+        }
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+                }
+            }
+        }
+        return dp[m][n];
     }
 }
