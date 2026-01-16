24e8161b2a2e7c86af851f00f4698458f44ff1b9
ritesh <riteshswain1973@gmail.com>
2026-01-16 23:28:13 +0530

    Candy

---
+++ Array/Candies.java
@@ -0,0 +1,10 @@
+public class Candies {
+    public static void main(String[] args) {
+        int[] nums={1,2,0};
+        System.out.println(candy(nums));
+    }
+    public static int candy(int[] nums){
+        int sum=0;
+        return sum;
+    }
+}
