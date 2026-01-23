6e921806abe454ebda84c9b36c47b836381f3a8b
ritesh <riteshswain1973@gmail.com>
2026-01-23 23:41:12 +0530

    Books

--- TwoPointer/Books.java
+++ TwoPointer/Books.java
@@ -1,7 +1,5 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
 import java.util.Scanner;
-class Books {
+public class Books {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
