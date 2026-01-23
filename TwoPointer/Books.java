481ef1f91e199fff480ed2deb890929e8969c587
ritesh <riteshswain1973@gmail.com>
2026-01-23 23:36:49 +0530

    Books

---
+++ TwoPointer/Books.java
@@ -0,0 +1,23 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class Books {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int t=sc.nextInt();
+        int[] nums=new int[n];
+        for(int i=0; i<n; i++){
+            nums[i]=sc.nextInt();
+        }
+        int max=0,sum=0,j=0;
+        for(int i=0; i<n; i++){
+            sum+=nums[i];
+            while(sum>t){
+                sum-=nums[j++];
+            }
+            max=Math.max(max,i-j+1);
+        }
+        System.out.println(max);
+    }
+}
