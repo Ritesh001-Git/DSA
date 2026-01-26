1fc35d435744d0b995dcdb3c7a3bd5a2c430bf0a
ritesh <riteshswain1973@gmail.com>
2026-01-26 23:57:45 +0530

    Again Twenty Five

--- Math/AgainTwentyFive.java
+++ Math/AgainTwentyFive.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class AgainTwentyFive {
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+        System.out.println(25);
     }
 }
