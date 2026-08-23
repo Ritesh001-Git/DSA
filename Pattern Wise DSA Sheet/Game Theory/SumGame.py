dc9bfcd242aa0eabd9e5b3f67291f991706fae89
Ritesh Swain <152496676+Ritesh001-Git@users.noreply.github.com>
2026-08-24 00:28:34 +0530

    Create SumGame.py

---
+++ Pattern Wise DSA Sheet/Game Theory/SumGame.py
@@ -0,0 +1,29 @@
+# TC - O(n)
+# SC - O(1)
+
+class Solution:
+    def sumGame(self, num: str) -> bool:
+        n = len(num)
+
+        leftSum = 0
+        rightSum = 0
+        
+        leftQ = 0
+        rightQ = 0
+
+        for i in range(n):
+            if num[i] == "?":
+                if i < n // 2:
+                    leftQ += 1
+                else:
+                    rightQ += 1
+            else:
+                if i < n // 2:
+                    leftSum += int(num[i])
+                else:
+                    rightSum += int(num[i])
+
+        totalQ = leftQ + rightQ
+        if totalQ % 2 == 1: return True # If total Question Mark is odd then Alice Wins
+
+        return 2 * leftSum + 9 * leftQ != 2 * rightSum + 9 * rightQ
